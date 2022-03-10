ae05f37bd79a3c05c5bfb4fb527cdcea038d3c9f
improve777 <improve777@gmail.com>
2022-03-11 00:43:27 +0900

    기둥과 보 설치 문제 풀이 추가

---
+++ implementation/pillar_and_beem_construction.py
@@ -0,0 +1,46 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    'n, build_frame, expected',
+    [(5,
+      [[1, 0, 0, 1], [1, 1, 1, 1], [2, 1, 0, 1], [2, 2, 1, 1], [5, 0, 0, 1], [5, 1, 0, 1], [4, 2, 1, 1], [3, 2, 1, 1]],
+      [[1, 0, 0], [1, 1, 1], [2, 1, 0], [2, 2, 1], [3, 2, 1], [4, 2, 1], [5, 0, 0], [5, 1, 0]]
+      ),
+     (5,
+      [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1], [2, 0, 0, 0],
+       [1, 1, 1, 0], [2, 2, 0, 1]],
+      [[0, 0, 0], [0, 1, 1], [1, 1, 1], [2, 1, 1], [3, 1, 1], [4, 0, 0]]
+      ),
+     ]
+)
+def test(n, build_frame, expected):
+    result = solution(n, build_frame)
+    assert result == expected
+
+
+def possible(answer):
+    for x, y, stuff in answer:
+        if stuff == 0:
+            if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y - 1, 0] in answer:
+                continue
+            return False
+        else:
+            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+                continue
+            return False
+    return True
+
+
+def solution(n, build_frame):
+    answer = []
+    for x, y, stuff, operate in build_frame:
+        if operate == 0:
+            answer.remove([x, y, stuff])
+            if not possible(answer):
+                answer.append([x, y, stuff])
+        if operate == 1:
+            answer.append([x, y, stuff])
+            if not possible(answer):
+                answer.remove([x, y, stuff])
+    return sorted(answer)
